1b8903507eefceecfb9835e7cb15b4ef623a7b1e
robynainsley21 <robynainsley2i@gmail.com>
2024-05-14 14:07:39 +0200

    Complete exercise

---
+++ script/script.js
@@ -0,0 +1,17 @@
+//question 1
+let firstName = 'Robyn'
+let lastName = 'Carnow'
+
+//question 2
+console.log(firstName + ' ' + lastName)
+
+//question 3
+let numberVar = 5
+
+//question 4
+//the output was 25
+
+//question 5
+const myName = 'Robyn'
+myName = lastName
+
